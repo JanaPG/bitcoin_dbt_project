d4d3466cb9bcb59d2c46eb9561a832e3b010e5a9
Jana <janarthanan0506@gmail.com>
2026-08-08 21:45:34 +0530

    implemented incremental load for stg_btc

--- models/stg_btc.sql
+++ models/stg_btc.sql
@@ -1,1 +1,22 @@
+{{
+config(
+materialized='incremental',
+incremental_strategy='append',
+unique_key='HASH_KEY'
+
+
+
+)
+
+
+
+}}
+
 select * from {{ source ('btc','btc') }}
+
+{% if is_incremental() %}
+where block_timestamp > (
+select max(block_timestamp) from {{ this }}
+
+)
+{% endif %}
