6e62f8f7bbc2944d435045092ec918b7b4fa7b84
Jana <janarthanan0506@gmail.com>
2026-08-16 01:35:37 +0530

    added a new model to flatten outputs column from stg_btc

--- models/stg_btc_outputs.sql
+++ models/stg_btc_outputs.sql
@@ -1,4 +1,11 @@
+{{
+config(
+materialized='incremental',
+incremental_strategy='append')
+}}
 
+
+with btc_flatten as (
 select
 tx.hash_key,
 tx.block_number,
@@ -11,3 +18,20 @@ from {{ref('stg_btc')}} tx,
 lateral flatten (input => outputs) f
 
 WHERE f.value:address::STRING IS NOT NULL
+
+{% if is_incremental() %}
+and block_timestamp > (
+select max(block_timestamp) from {{ this }}
+
+)
+{% endif %}
+)
+
+select 
+hash_key,
+block_number,
+block_timestamp,
+is_coinbase,
+output_address,
+output_value
+from btc_flatten
