3516dedcf752003b50f65426155d352c97aa8859
Jana <janarthanan0506@gmail.com>
2026-08-17 01:34:38 +0530

    dbt seed for usd value and added runbook for snowflake trial expired issue

--- models/Marts/whale_alert.sql
+++ models/Marts/whale_alert.sql
@@ -1,4 +1,4 @@
-select
+with whale as (select
 output_address,
 sum(output_value) as total_sent,
 count(*) as tx_count
@@ -9,3 +9,17 @@ where output_value > 10
 
 group by output_address
 order by total_sent desc
+),
+ latest as (
+select * 
+from {{ref('btc_usd_max')}}
+where to_date(replace(event_date,' UTC','')) = current_date()-1
+
+)
+select 
+w.output_address,
+w.total_sent,
+w.tx_count,
+(p.close_price_usd * w.total_sent) as total_sent_usd
+from WHALE w
+cross join LATEST p
